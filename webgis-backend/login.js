ceb147d551c5542ded1b8e38970d8b6acffb1e6a
DutyKendartiwastra <101196076+DutyKendartiwastra@users.noreply.github.com>
2026-06-21 21:33:22 +0700

    commit fetch backend

--- webgis-backend/login.js
+++ webgis-backend/login.js
@@ -8,7 +8,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     errorMsg.style.display = "none";
 
     try {
-        const response = await fetch('http://localhost:3000/api/login', {
+        const response = await fetch('https://backendwebgis-production.up.railway.app', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
